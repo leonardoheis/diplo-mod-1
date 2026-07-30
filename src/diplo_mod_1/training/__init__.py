4a4b53230cd44964e892b8adfad1755a600810b2
Leonardo Heis <leonardo.heis@gmail.com>
2026-07-30 16:50:02 -0300

    feat: export NN training classes from training package

--- src/diplo_mod_1/training/__init__.py
+++ src/diplo_mod_1/training/__init__.py
@@ -1,4 +1,4 @@
-"""XGBoost hyperparameter tuning package."""
+"""XGBoost and PyTorch NN training package."""
 
 from diplo_mod_1.training.config import (
     RunRecord,
@@ -7,13 +7,23 @@ from diplo_mod_1.training.config import (
     XGBoostTuningConfig,
 )
 from diplo_mod_1.training.device import detect_torch_device, detect_xgboost_device
+from diplo_mod_1.training.nn_config import NNSearchSpace, NNTuningConfig
+from diplo_mod_1.training.nn_model import WineScoreNet, WineScorePredictorNet
+from diplo_mod_1.training.nn_registry import NNModelRegistry
+from diplo_mod_1.training.nn_tuner import NNTuner
 from diplo_mod_1.training.registry import ModelRegistry
 from diplo_mod_1.training.xgboost_tuner import XGBoostTuner
 
 __all__ = [
     "ModelRegistry",
+    "NNModelRegistry",
+    "NNSearchSpace",
+    "NNTuner",
+    "NNTuningConfig",
     "RunRecord",
     "TuningHistory",
+    "WineScoreNet",
+    "WineScorePredictorNet",
     "XGBoostSearchSpace",
     "XGBoostTuner",
     "XGBoostTuningConfig",
